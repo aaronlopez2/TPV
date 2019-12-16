588484faa3c72abe4be5be1951ef5be0bf1363c2
Aarón López López <alopezl@red.local>
2019-12-16 17:48:02 +0100

    loginCasiAutentication

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,33 @@
 package sample;
 
+import javafx.scene.input.KeyCode;
+
+import javax.crypto.spec.PBEParameterSpec;
+import java.awt.event.KeyEvent;
+
+
 public class Controller {
+
+    Modelo model = new Modelo();
+
     public void mensaje(){
         System.out.println("PRUEBA");
+    }
+
+    public String Aceptar(String user, String pwd) {
+        boolean log = model.Autenticar(user,pwd);
+        if(log) {
+            return "Bien";
+        } else {
+            return "Fallo";
+        }
+    }
+    public void autenticacion(String aut){
+        if(aut.equals("Bien")) {
+
+        } else if(aut.equals("Fallo")) {
+
+        }
+
     }
 }
